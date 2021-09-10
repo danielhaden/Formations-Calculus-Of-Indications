b966fcac1eb9a9e9376cea420891ce44790422c0
Daniel Haden <haden.daniel@gmail.com>
2021-09-10 14:08:12 -0600

    BaseViewModel class and expression helper functions

--- Formations/MainWindow.xaml.cs
+++ Formations/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Formations
+namespace Formations.Engine
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,6 +23,7 @@ namespace Formations
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainWindowViewModel(this);
         }
     }
 }
---
+++ Formations/View Models/MainWindowViewModel.cs
@@ -0,0 +1,53 @@
+﻿namespace Formations.Engine
+{
+    using Formations.Engine.Core;
+    using System.Windows;
+
+    internal class MainWindowViewModel : BaseViewModel
+    {
+
+        #region "Private Members"
+
+        /// <summary>
+        /// The <see cref="Window"/> this view model controls
+        /// </summary>
+        private Window MyWindow;
+
+        #endregion
+
+        #region "Public Properties"
+
+        /// <summary>
+        /// The minimum window height that can be set by the user
+        /// </summary>
+        public int WindowMinimumHeight { get; set; } = 500;
+
+        /// <summary>
+        /// The minimum window width that can be set by the user
+        /// </summary>
+        public int WindowMinimumWidth { get; set; } = 800;
+
+
+        private int _WindowRadius = 10;
+        public int WindowRadius
+        {
+            get => _WindowRadius;
+            set => _WindowRadius = value;
+        }
+
+
+        #endregion
+
+        #region "Constructors"
+
+        public MainWindowViewModel(Window window)
+        {
+            MyWindow = window;
+        }
+
+        #endregion
+
+
+
+    }
+}
